f8f2c37d5660843ecc00d2e13f889a085f59c0f3
mocosvl2 <mriveraconchas1995@gmail.com>
2026-02-27 04:24:49 -0600

    Create app/page.tsx file with the LandingCurso component

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingCurso = () => {
+    return (
+        <div>
+            <h1>Welcome to the Landing Curso!</h1>
+            <p>This is a brief description of the curso.</p>
+        </div>
+    );
+};
+
+export default LandingCurso;
